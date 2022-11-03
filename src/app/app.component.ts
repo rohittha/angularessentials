4f2d65c6d015e6c084276f6e6696bff4b714415f
rohittha <SKULLSHOXHMS@GMAIL.COM>
2022-11-03 11:00:29 -0400

    InitialCommit2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
+// Component decorator takes some known properties. Selector and Template are mandatory.
 @Component({
-  selector: 'app-root',
+  selector: 'rt-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls:  ['./app.component.css']
 })
-export class AppComponent {
-  title = 'essentialtrain';
-}
+// Export is needed to be added so that we can use this component somewhere else. 
+export class AppComponent{}
+
+
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css']
+// })
+// export class AppComponent {
+//   title = 'essentialtrain';
+// }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,36 @@
 import { NgModule } from '@angular/core';
+//Since we want to use the app in a browser we need the BrowserModule
+//Contains core directives, pipes and more for working with the DOM.
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+//Instead of a module, we can load directly from a file
 import { AppComponent } from './app.component';
+import { MediaItemComponent } from './media-item/media-item.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
+  //Bring other modules that this module needs
+  imports: [BrowserModule],
+  //Bring in component, directives and pipes avaiable to this module
+  declarations: [AppComponent, MediaItemComponent],
+  //components that will be starting point
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule{}
+
+// import { NgModule } from '@angular/core';
+// import { BrowserModule } from '@angular/platform-browser';
+
+// import { AppRoutingModule } from './app-routing.module';
+// import { AppComponent } from './app.component';
+
+// @NgModule({
+//   declarations: [
+//     AppComponent
+//   ],
+//   imports: [
+//     BrowserModule,
+//     AppRoutingModule
+//   ],
+//   providers: [],
+//   bootstrap: [AppComponent]
+// })
+// export class AppModule { }
